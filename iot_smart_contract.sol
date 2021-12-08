a5daa45c1673b7ec7c117d1894c6ef2c70c858b0
WilliamBaltus <wbaltus21@gmail.com>
2021-12-08 18:11:47 -0500

    Update iot_smart_contract.sol

--- iot_smart_contract.sol
+++ iot_smart_contract.sol
@@ -15,16 +15,14 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 contract APIConsumer is ChainlinkClient {
     using Chainlink for Chainlink.Request;
   
-    uint256 public volume;
-    
+    uint256 public temperature;
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
     
     /**
      * Network: Kovan
-     * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Devrel   
-     * Node)
+     * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Node)
      * Job ID: d5270d1c311941d0b08bead21fea7747
      * Fee: 0.1 LINK
      */
@@ -37,30 +35,20 @@ contract APIConsumer is ChainlinkClient {
     
     /**
      * Create a Chainlink request to retrieve API response, find the target
-     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
+     * data,
      */
     function requestVolumeData() public returns (bytes32 requestId) 
     {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
         
         // Set the URL to perform the GET request on
-        request.add("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD");
+        request.add("get", "https://iot-smart-contract.herokuapp.com/");
         
         // Set the path to find the desired data in the API response, where the response format is:
-        // {"RAW":
-        //   {"ETH":
-        //    {"USD":
-        //     {
-        //      "VOLUME24HOUR": xxx.xxx,
-        //     }
-        //    }
-        //   }
-        //  }
-        request.add("path", "RAW.ETH.USD.VOLUME24HOUR");
-        
-        // Multiply the result by 1000000000000000000 to remove decimals
-        int timesAmount = 10**18;
-        request.addInt("times", timesAmount);
+        // {"Device":"Raspberry Pi",
+        //  "Temperature":"73.2"}
+        //  
+        request.add("path", "Temperature");
         
         // Sends the request
         return sendChainlinkRequestTo(oracle, request, fee);
@@ -69,9 +57,9 @@ contract APIConsumer is ChainlinkClient {
     /**
      * Receive the response in the form of uint256
      */ 
-    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId)
+    function fulfill(bytes32 _requestId, uint256 _temperature) public recordChainlinkFulfillment(_requestId)
     {
-        volume = _volume;
+        temperature = _temperature;
     }
 
     // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
