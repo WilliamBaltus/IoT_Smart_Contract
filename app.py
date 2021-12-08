8a87736e19874726854800f042bcafe2e76fe94c
WilliamBaltus <wbaltus21@gmail.com>
2021-12-08 12:48:43 -0500

    firebase connection to heroku

--- app.py
+++ app.py
@@ -11,13 +11,26 @@ from flask import Flask, jsonify
 from time import sleep
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
 
 
 app = Flask(__name__)
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(r"iot_smart_contract.json")
+    credentialsDict = {"type": os.environ['type'],
+                        "project_id":os.environ['project_id'],
+                        "private_key_id":os.environ['private_key_id'],
+                        "private_key":os.environ['private_key'],
+                        "client_email":os.environ['client_email'],
+                        "client_id": os.environ['client_id'],
+                        "auth_uri":os.environ['auth_uri'],
+                        "token_uri":os.environ['token_uri'],
+                        "auth_provider_x509_cert_url":os.environ['auth_provider_x509_cert_url'],
+                        "client_x509_cert_url":os.environ['client_x509_cert_url']
+    }
+    cred = credentials.Certificate(credentialsDict)
     firebase_admin.initialize_app(cred)
+    
 
 firebase= firestore.client()
 
